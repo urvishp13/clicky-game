cf4c4e348d695ccfba8a7a8e8f172ee0664768f8
Urvish Patel <urvishp13@gmail.com>
2018-11-13 14:13:51 -0500

    Updates

---
+++ precache-manifest.9c5a5b77cf198ba1f8beec788ea6f51c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "97e996bd216b2fa4a37f",
+    "url": "/clicky-game/static/js/main.97e996bd.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/clicky-game/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "97e996bd216b2fa4a37f",
+    "url": "/clicky-game/static/css/main.635c5c74.chunk.css"
+  },
+  {
+    "revision": "971a62e4675db1435e66f332a44d992c",
+    "url": "/clicky-game/index.html"
+  }
+];
